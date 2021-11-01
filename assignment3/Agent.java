5b1694915fece41d6c7bf557ef3d4112ada80182
jedsaro <jedsaro@gmail.com>
2021-11-01 02:43:32 -0700

    final, hope it works :s

--- assignment3/Agent.java
+++ assignment3/Agent.java
@@ -1,114 +1,23 @@
-import java.io.*;
-import java.util.ArrayList;
-//import java.util.Iterator;
+import java.util.Stack;
 
 public class Agent {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
-        Agent evilme = new Agent();
-        ArrayList<String> list = new ArrayList<>();
+        Stack<String> stack = new Stack<String>();
 
-        list = evilme.filetoArray();
-
-        evilme.run(args, list);
-
-    }
-
-    // =======================================================
-
-    public void run(String [] args, ArrayList<String> list) {
-
-        double percentageC = getConfessions(list)/list.size();
+        args = new String[1];
         
-        inputReader(args);
+        stack.push("silent");
+        stack.push("confess");
 
-        if(percentageC > 0.3)
+        if(args[0] == "silent")
         {
-            alwaysSilent();
-            
+            System.out.println(stack.peek());
         }
         else
         {
-            alwaysConfess();
-        }
-
-    }
-
-    public void alwaysConfess() 
-    {
-        System.out.println("confess");
-    }
-
-    public void alwaysSilent()
-    {
-        System.out.println("silent");
-    }
-
-    // =============================================================================================
-    // tools
-
-    public double getSilent(ArrayList <String>  methodArray) {
-
-        double total = 0;
-
-        for (int i = 0; i < methodArray.size(); i++) {
-            if (methodArray.get(i).equals("silent")) {
-                total++;
-
-            }
-        }
-
-        return total;
-    }
-
-    public double getConfessions(ArrayList <String> methodArray) {
-
-        double total = 0;
-
-        for (int i = 0; i < methodArray.size(); i++) {
-            if (methodArray.get(i).equals("confess")) {
-                total++;
-
-            }
-        }
-
-        return total;
-    }
-
-    // reads txt file and places it into an array
-    public ArrayList<String> filetoArray() throws IOException {
-
-        BufferedReader readtext = new BufferedReader(new FileReader("Enemy_Input.txt"));
-
-        String str;
-
-        ArrayList<String> enemyInput = new ArrayList<String>();
-
-        while ((str = readtext.readLine()) != null) {
-
-            enemyInput.add(str);
-        }
-
-        return enemyInput;
-    }
-
-    // reads user input and stores it into a txt file
-    public void inputReader(String[] args) {
-
-        try {
-
-            BufferedWriter file = new BufferedWriter(new FileWriter("Enemy_Input.txt", true));
-            String val = args[0];
-            file.write(val);
-            file.newLine();
-            file.close();
-
-            System.out.print(args[0]);
-
-        } catch (IOException e) {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
+            System.out.println(stack.pop());
         }
 
     }
