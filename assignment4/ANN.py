ae85f1211c53ff3a91c1f8cddaeb4a27db60f1a1
Jesus Sanchez <33048003+jedsaro@users.noreply.github.com>
2021-11-07 12:20:17 -0800

    testingx2

--- assignment4/ANN.py
+++ assignment4/ANN.py
@@ -1,1 +1,11 @@
 class Node:
+
+    def __init__(self):
+        
+        self.children = []
+        self.node_name = "taco"
+        self.children_connection_weights = []
+
+    def make_children(self, current_layer, nodes_per_layer_map):
+        
+
