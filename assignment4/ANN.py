78765cd92d664acbbd26057a8ab14f47dcf62953
Jesus Sanchez <33048003+jedsaro@users.noreply.github.com>
2021-11-11 08:10:45 -0800

    working

--- assignment4/ANN.py
+++ assignment4/ANN.py
@@ -3,9 +3,21 @@ class Node:
     def __init__(self):
         
         self.children = []
-        self.node_name = "taco"
+        self.name = "taco"
         self.children_connection_weights = []
 
     def make_children(self, current_layer, nodes_per_layer_map):
+        if current_layer == len(nodes_per_layer_map):
+            return
+
+        for i in range( nodes_per_layer_map[current_layer] ):
+            self.children.append(Node())
+            
+
+
+
+    
+
+    
         
 
