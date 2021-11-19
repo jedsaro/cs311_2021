13661a16834704c5c36b88fca349bb1822dfa204
Jesus Sanchez <33048003+jedsaro@users.noreply.github.com>
2021-11-19 13:20:49 -0800

    commit

--- assignment4/ANN.py
+++ assignment4/ANN.py
@@ -3,33 +3,38 @@ import random
 GLOBAL_MAP =  [4,3,2]
 
 class Node:
+
     def __init__(self, name):
         
         self.children = [] 
         self.weight = []
         self.name = name
 
-    def make_children(self, current_layer_number, nodes_per_layer_map):
+    def make_children(self, current_layer, nodes_per_layer):
 
-        if current_layer_number >= len(nodes_per_layer_map):
+        #last node terminates child creation
+        if current_layer >= len(nodes_per_layer):
             return
 
-        for i in range(nodes_per_layer_map[current_layer_number]):
+        #populates nodes per layer and sends increment of char (recursion)
+        for i in range(nodes_per_layer[current_layer]):
 
             self.children.append(Node(chr(ord(self.name) + 1)))
 
-        #recursion
-        self.children[0].make_children(current_layer_number + 1 , nodes_per_layer_map)
+        #moves to next layer and 
+        self.children[0].make_children(current_layer + 1 , nodes_per_layer)
 
         for i in range(1, len(self.children)):
+
             self.children[i].children = self.children[0].children[:]
 
 
-    def prety_print(self, current_layer_number, node_per_layer_map):
+    #function output
+    def print(self, current_layer, node_per_layer):
         
-        indent = '   ' * current_layer_number #output organizor
+        indent = '   ' * current_layer 
 
-        if current_layer_number >= len(node_per_layer_map):
+        if current_layer >= len(node_per_layer):
 
             print(f"{indent} {self.name}")
             return 
@@ -44,21 +49,24 @@ class Node:
             except:
                 pass
 
-            self.children[i].prety_print(current_layer_number + 1, node_per_layer_map)
+            self.children[i].print(current_layer + 1, node_per_layer)
 
         return 
 
-    def set_random_weights(self, current_layer_number, node_per_layer_map):
-        if current_layer_number >= len(node_per_layer_map):
+    def random_weights(self, current_layer, node_per_layer):
+
+        #last node terminates weight creation
+        if current_layer >= len(node_per_layer):
             return
 
         self.weight = [0.0] * len(self.children)
 
         for i in range(len(self.children)):
 
+            #storing random numbers between 0 and 1
             self.weight[i] = random.uniform(0, 1)
 
-            self.children[i].set_random_weights(current_layer_number + 1, node_per_layer_map)
+            self.children[i].random_weights(current_layer + 1, node_per_layer)
 
         return
 
@@ -67,12 +75,12 @@ if __name__ == '__main__':
 
     node = Node("A")
     node.make_children(0, GLOBAL_MAP)
-    node.prety_print(0, GLOBAL_MAP)
+    node.print(0, GLOBAL_MAP)
 
     print("After: ")
 
-    node.set_random_weights(0, GLOBAL_MAP)
-    node.prety_print(0, GLOBAL_MAP)
+    node.random_weights(0, GLOBAL_MAP)
+    node.print(0, GLOBAL_MAP)
 
 
 
