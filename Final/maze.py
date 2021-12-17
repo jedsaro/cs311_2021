e7ebc398c7dcbfce274e05af7f42693e0aa32fa5
Jesus Sanchez <33048003+jedsaro@users.noreply.github.com>
2021-12-17 09:20:12 -0800

    secs fix

--- Final/maze.py
+++ Final/maze.py
@@ -248,7 +248,7 @@ if __name__ == '__main__':
   t1_start = process_time_ns()
   dijkstra(maze);
   t1_stop = process_time_ns()
-  print("\nTime Taken (Dijkstra): \n ", t1_stop-t1_start)
+  print("\nTime Taken (Dijkstra): \n ", (t1_stop-t1_start)/1000000000, "Secs")
 
 
   enter_pause = input()
@@ -258,4 +258,4 @@ if __name__ == '__main__':
   t2_start = process_time_ns()
   Astar(maze);
   t2_stop = process_time_ns()
-  print("\nTime Taken (A*):\n ", (t2_stop-t2_start))
+  print("\nTime Taken (A*):\n ", (t2_stop-t2_start)/1000000000, "Secs")
